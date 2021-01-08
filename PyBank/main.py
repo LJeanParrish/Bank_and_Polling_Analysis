c2915a77197626ffa19034c986587b395c92dd67
Lauren Parrish <ljeanparrish@gmail.com>
2021-01-08 10:07:55 -0500

    Additional code options for pnl

--- PyBank/main.py
+++ PyBank/main.py
@@ -6,7 +6,7 @@ import csv
 csvpath = os.path.join('..', 'PyBank', 'Resources', 'budget_data.csv')
 
 #Define lists to store data
-month = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+#month = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
 Total_PNL = []
 
 print("Financial Analysis")
@@ -25,20 +25,33 @@ with open(csvpath) as csvfile:
     for row in csvreader:
         row_count = sum(1 for row in csvreader)
         print(f'Total Months: {1 + row_count}')
-        
-        
-        pnl = [row[1]]
-        Total_PNL.append([int(pnl)])
-        print(Total_PNL)
+        print(row[1:])  #test code
 
         #Part III - Total the net amount of profits and losses
-        # #Loop through row[2] and add all data points and print total
+        #Loop through row[1] and add all data points and print total
+        
+        
+        
+        
+        Total_PNL = sum(float(row[1]) for row in csvreader)
+        print(f'Total: {Total_PNL}')        
+        
+        
+        #Part IV - Calculate the changes in "Profit/Losses" over the entire period,
 
 
 
 
 
-#Part IV - Calculate the changes in "Profit/Losses" over the entire period,
+
+
+
+
+
+
+
+
+
 # Then find the average of those changes
 #Part V - The greatest increase in profits (date and amount) over the entire period
 #PartVI - The greatest decrease in losses (date and amount) over the entire period
