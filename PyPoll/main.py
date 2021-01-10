1b875c0d651e4ec75f51f45036b20187c91b5c99
Lauren Parrish <ljeanparrish@gmail.com>
2021-01-10 11:23:57 -0500

    Updated Election Results Script

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,11 +1,12 @@
-Part I - Create script to import data from CSV file
+#Part I - Create script to import data from CSV file
 #Set modules for importing the data
 import os
 import csv
 
-csvpath = os.path.join('..', 'PyPoll', 'Resources', 'election_data')
+csvpath = os.path.join('..', 'PyPoll', 'Resources', 'election_data.csv')
 
 #Define lists to store data
+row_count = 0
 
 
 print("Election Results")
@@ -20,7 +21,9 @@ with open(csvpath) as csvfile:
     # Read each row of data after the header
     csv_header = next(csvreader)        
     
-    #Calculate the total number of votes and add to row_count list
+    #Calculate the total number of individual votes and add to row_count list
     for row in csvreader:
-        #row_count = sum(1 for row in csvreader)
-        #print(f'Total Votes: {1 + row_count}')
+        row_count = sum(1 for row in csvreader)
+        
+        
+print(f'Total Votes: {1 + row_count}')
