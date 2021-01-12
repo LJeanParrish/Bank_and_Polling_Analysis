f7d4b99a700c7270f78ad8d36817b7c9b27a69bc
Lauren Parrish <ljeanparrish@gmail.com>
2021-01-12 18:57:19 -0500

    Final code for PyBank analysis

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,4 +1,4 @@
-#Part I - Create script to import data from CSV file
+#Create script to import data from CSV file
 #Set modules for importing the data
 import os
 import csv
@@ -17,7 +17,7 @@ PNL_Diff = 0
 print("Financial Analysis")
 print("-------------------------")
 
-#Part II - Loop through data set to print the calculations
+# Loop through data set to print the calculations
 # open csv file path
 
 with open(csvpath) as csvfile:
@@ -26,11 +26,11 @@ with open(csvpath) as csvfile:
     # Read each row of data after the header
     csv_header = next(csvreader)        
     
-    #Part III - Calculate the total number of months and add to row_count list
+    #Calculate the total number of months and add to row_count list
     for row in csvreader:    
         row_count = row_count + 1
         
-        #Part IV - Calculate the changes in "Profit/Losses" over the entire period,
+        #Calculate the changes in "Profit/Losses" over the entire period,
         Total_PNL = Total_PNL + int(row[1])
         PNL_Diff = int(row[1]) - previous
 
@@ -38,13 +38,13 @@ with open(csvpath) as csvfile:
         Avg_Change.append(PNL_Diff)
         previous = int(row[1])
 
-        #Part V - The greatest increase in profits (date and amount) over the entire period
+        #Identify the greatest increase in profits (date and amount) over the entire period
 
         if PNL_Diff > Max_Profit[1]:
             Max_Profit[1] = PNL_Diff 
             Max_Profit[0] = row[0]
 
-         #PartVI - The greatest decrease in losses (date and amount) over the entire period
+         #Identify the greatest decrease in losses (date and amount) over the entire period
 
         if PNL_Diff < Min_Profit[1]:
             Min_Profit[1] = PNL_Diff 
@@ -54,30 +54,20 @@ with open(csvpath) as csvfile:
     Total_Avg = sum(Avg_Change[1:])/(len(Avg_Change)-1)
     Total_Avg =str(round(Total_Avg,2))
 
-    # #Print all findings of Financial Analysis
-    # print(f'Total Months: {row_count}')        
-    # print(f'Total: ${Total_PNL}')
-    # print(f'Average Change: ${Total_Avg}')
-    # print(f'Greatest Increase in Profits: {Max_Profit[0]}, (${Max_Profit[1]})')
-    # print(f'Greatest Decrease in Profits: {Min_Profit[0]}, (${Min_Profit[1]})')
+    #Print Financial Analysis findings to Bash terminal
     results = (f'Total Months: {row_count}\n'
     f'Total: ${Total_PNL}\n'
     f'Average Change: ${Total_Avg}\n'
     f'Greatest Increase in Profits: {Max_Profit[0]}, (${Max_Profit[1]})\n'
     f'Greatest Decrease in Profits: {Min_Profit[0]}, (${Min_Profit[1]})')
+    
     print(results)
-#Part VII - specify the file export the Financial Data to write as txt file)
+
+#Specify the file and export the Financial Data to write as txt file)
 # Set variable for output file
 output_file = os.path.join('..', 'Pybank', 'Analysis',"Financial_Analysis.txt")
 
 # Open the output file
 with open(output_file, "w", newline="") as txtfile:
-    # writer.writerow(["Financial Analysis"])
-    # writer.write([f'Total Months: {row_count}\n'])
-    # writer.writerow(["Total: $38382578"])
-    # writer.writerow(["Average Change: $-2315.12"])
-    # writer.writerow(["Greatest Increase in Profits: Feb-2012, ($1926159)"])
-    # writer.writerow(["Greatest Decrease in Profits: Sep-2013, ($-2196167)"])
-
     txtfile.write(results)
    
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,4 +1,4 @@
-#Part I - Create script to import data from CSV file
+#Create script to import data from CSV file
 #Set modules for importing the data
 import os
 import csv
@@ -12,7 +12,7 @@ canidate_list = []
 print("Election Results")
 print("-------------------------")
 
-#Part II - Retrieve data from CSV file path
+#Retrieve data from CSV file path
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',') 
       
@@ -27,143 +27,54 @@ with open(csvpath) as csvfile:
         #Calculate the total number of individual votes in the election
         row_count = row_count + 1 
 
-        #Loop through data set and add canidates to canidate list
-        #Add the number of votes each canidate received
-        
+        #Loop through data set and add canidates to canidate list      
         if row[2] not in canidate_list:
             canidate_list.append(row[2])
             canidatedict[row[2]] = 0
 
+        #Add the number of votes each canidate received 
         canidatedict[row[2]] = canidatedict[row[2]] + 1
     
+    #print election results
     print(f'Total Votes: {row_count}')    
     print("-------------------------")
-    print(canidate_list)    
-    print(canidatedict)
-#         #Capture the canidate list
-#         #Calculate the total of votes each canidate won
+    #print(f'{canidate_list}: {canidatedict}')
+    #print("-------------------------")
+    
+    #print(canidate_list)    
+    #print(canidatedict)
+
+   
     canidateperdict = {} 
     maxcount = 0
-    for canidate in canidatedict:
-        canidateperdict[canidate] = round((canidatedict[canidate]/row_count * 100), 2)
-        #print(canidate)
-        print(f'{canidate}, {canidateperdict[canidate]}%, {canidatedict[canidate]}')
 
+    for canidate in canidatedict:
+        #canidateperdict[canidate] = round((canidatedict[canidate]/row_count * 100), 3) 
+        canidateperdict[canidate] = str(round((canidatedict[canidate]/row_count * 100), 3))         
+        print(f'{canidate}: {canidateperdict[canidate]}% ({canidatedict[canidate]})')
+
+         #Determine which canidate is the winner
         if canidatedict[canidate] > maxcount:
             maxcount = canidatedict[canidate]
             winner = canidate
-    print(winner)
 
+    print("-------------------------")
+    print(f'Winner: {winner}')
+    print("-------------------------")
 
+    results = (f'Total Votes: {row_count}\n'
+    f'{"-------------------------"}\n'
+    f'{canidate}: {canidateperdict[canidate]}% ({canidatedict[canidate]})\n'
+    f'{"-------------------------"}\n'
+    f'Winner: {winner}\n'
+    f'{"-------------------------"}')
 
-#         khan_count = 
-#         correy_count =
-#         li_count =
-#         otooley_count = 
-    
-       
+#Specify the file export for the election results to write as txt file)
+#Set variable for output file
+output_file = os.path.join('..', 'Pypoll', 'Analysis',"Election_Results.txt")
 
-#         #Calculate the percentage of votes each candidate won
-#         kpercent = (khan_count)/(row_count)
-#         kpercent =str(round(kpercent,2))
-
-#         cpercent = (correy_count)/(row_count)
-#         cpercent =str(round(cpercent,2))
-
-#         lpercent = (li_count)/(row_count)
-#         lpercent =str(round(cpercent,2))
-
-#         opercent = (otooley_count)/(row_count)
-#         opercent =str(round(cpercent,2))
-
-
-# #Create a dictionary which holds all election results
-# election_results = {['canidate': "Khan", 'percent': kperent, 'vote': khan_count],
-# ['canidate': "Correy", 'percent': cperent, 'vote' : correy_count],
-# ['canidate': "Li", 'percent': lperent, 'vote': li_count],
-# ['canidate': "O'Tooley", 'percent': operent, 'vote': otooley_count]}
-
-# #Print all analysis on Election Results
-# print(f'Total Votes: {1 + row_count}')
-# print("-------------------------")
-# print(election_results)      
-
-
-# #The winner of the election based on popular vote
-# #Create conditional to determine election results
-# If khan_count > correy_count and li_count, and otooley_count:
-# print("Winner: Khan")
-
-# If correy_count > khan_count and li_count and otooley_count:
-# print("Winner: Correy")
-
-# If li_count > khan_count and correy_count and otooley_count:
-# print("Winner: Li")
-
-# If otooley_count  > khan_count and correy_count and li_count:
-# print("Winner: O'Tooley")
-
-    
-#     ###Other Attempts for part 2--------------------------------------------------
-#     #     if str(row[2]) == Khan:
-#     #         Khan_Vote.append(khan)
-#     # print(Khan_Vote)
-
-#     #option 4
-#      # khan_vote = khan_vote + int(row[2])
-#         # khan_count = int(row[2]) - previous
-#         # khan_total.append(khan_count)
-#         # previous = int(row[2])
-
-#     #option 5
-#         #Create a list which will store canidate names
-#         #  if canidate == Khanint([2]:
-#         #     khan_count = Khan + 1
-            
-#         # if canidate == Correy[2]:
-#         #     correy_count == Correy + 1
-
-#         # if canidate == Li[2]:
-#         #     li_count = Li + 1
-
-#         # if canidate == OTooley[2]:
-#         #     otooley_count = "O'Tooley" + 1          
-            
-
-#             # canidate_list.append(canidate)
-#             # previous = int(row[2])   
-        
-#         # #option 3
-#         # if canidate not in canidate_list:
-#         #     canidate_list.append(canidate)
-#         #     previous = str(row[2])
-        
-#         # #Option 2
-#         # canidate = canidate + str(row[2])
-#         # canidate_count = str(row[2]) - previous
-#         # canidate_list.append(canidate_count)
-#         # previous = str(row[2])
-        
-        
-#         # #Option 1
-#         # #if (row[2]) not in canidate_list:
-#         #     #canidate_list.append(row[2])
-#         #     # #previous = row[2]
-        
-
-
-
-# #Specify the file export the Financial Data to write as txt file)
-# # Set variable for output file
-# #output_file = os.path.join('..', 'Pypoll', 'Analysis',"Election_Results.csv")
-
-# # Open the output file
-# # with open(output_file, "w", newline="") as csvfile:
-# #     writer = csv.writer(csvfile)
-# #     writer.writerow(["Election Results"])
-# #     writer.writerow(["Total Votes: "])
-# #     writer.writerow([""])
-# #     writer.writerow([""])
-# #     writer.writerow([""])
-# #     writer.writerow([""])
+# Open the output file
+with open(output_file, "w", newline="") as txtfile:
+    txtfile.write("Election Results")
+    txtfile.write(results)
    
